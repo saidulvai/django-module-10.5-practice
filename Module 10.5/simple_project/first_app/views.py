c49fde851c4d30d6f6d66209a074d69d2f83aac4
saidulvai <saidulvaicsebgctub@gmail.com>
2024-02-06 00:42:20 +0600

    Module 10.5

--- Module 10.5/simple_project/first_app/views.py
+++ Module 10.5/simple_project/first_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import datetime
+def home(request):
+    d = {
+        'lst' : ['python', 'is', 'fun'],
+         
+         'date' : datetime.datetime.now(),
+         'add' : '5',
+         'capfirst' : 'django',
+         'cut' : "Python is Fun",
+         'dic' : [
+    {'name': 'Josh', 'age': 19},
+    {'name': 'Dave', 'age': 22},
+    {'name': 'Joe', 'age': 31},
+],
+        'firstFilter':  ['Apple', 'Mango', 'Orange'],
+        'lengthFilter' : ['Banana', 'Mango', 'Orange'],
+        'linenumbersFilter' : 'cat',
 
-# Create your views here.
+        'time' : '10:30:00.000123+02:00',
+         }
+    return render(request, 'home.html', d)
